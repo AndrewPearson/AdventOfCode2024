d7567d0d0339961e73ae2e2409850dc315786589
Andrew Pearson <AndrewPearson@users.noreply.github.com>
2024-12-03 19:35:55 +0000

    Day1 complete

---
+++ Day1/Day1/DataReader.cs
@@ -0,0 +1,21 @@
+﻿namespace Day1;
+
+public static class DataReader
+{
+    public static (List<long> First, List<long> Second) Read(string path)
+    {
+        using var r = new StreamReader(File.OpenRead(path));
+
+        var f = new List<long>();
+        var s = new List<long>();
+
+        while (r.ReadLine() is { } line)
+        {
+            var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            f.Add(int.Parse(nums[0]));
+            s.Add(int.Parse(nums[1]));
+        }
+
+        return (f, s);
+    }
+}
---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day1;
+
+var path = "input.txt";
+
+var (first, second) = DataReader.Read(path);
+Console.WriteLine($"{first.Count} rows read from '{path}'");
+
+var dist = CalculateDistance(first, second);
+Console.WriteLine($"Total Distance = {dist}");
+
+var similarity = CalculateSimilarity(first, second);
+Console.WriteLine($"Total Similarity = {similarity}");
+
+static long CalculateDistance(List<long> first, List<long> second)
+{
+    return first.Order().Zip(second.Order())
+        .Sum(static item => Math.Abs(item.First-item.Second));
+}
+
+static long CalculateSimilarity(List<long> first, List<long> second)
+{
+    var secondFreq = second
+        .CountBy(static x => x)
+        .ToDictionary(static x => x.Key, static x => x.Value);
+
+    return first
+        .Select(f => f * secondFreq.GetValueOrDefault(f, 0))
+        .Sum(static x => x);
+}
